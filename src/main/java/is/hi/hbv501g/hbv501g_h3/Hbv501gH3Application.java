39a4621bdf3a3f0a9b49751d339404f2ef73463a
snaeja <sea28@hi.is>
2024-09-07 17:05:54 +0000

    initial commit take 2

--- src/main/java/is/hi/hbv501g/hbv501g_h3/Hbv501gH3Application.java
+++ src/main/java/is/hi/hbv501g/hbv501g_h3/Hbv501gH3Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Hbv501gH3Application {
-	//this is my trivial change
+	//this is my trivial change 2
 	public static void main(String[] args) {
 		SpringApplication.run(Hbv501gH3Application.class, args);
 	}
