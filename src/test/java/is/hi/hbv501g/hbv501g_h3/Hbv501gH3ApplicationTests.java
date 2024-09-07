460634cc2aba06598f2557cc7c1eef77e86aa1ea
snaeja <sea28@hi.is>
2024-09-07 16:59:35 +0000

    initial commit

---
+++ src/test/java/is/hi/hbv501g/hbv501g_h3/Hbv501gH3ApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.hbv501g_h3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hbv501gH3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
