5bf89a55fef32d6b7146e5edb8f8fecd16fa8cf4
unknown <aroneidur@gmail.com>
2024-09-08 19:45:23 +0000

    init2

--- src/main/java/is/hi/hbv501g/hbv501g_h3/HelloController.java
+++ src/main/java/is/hi/hbv501g/hbv501g_h3/HelloController.java
@@ -1,4 +1,13 @@
 package is.hi.hbv501g.hbv501g_h3;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!!!!!!!!!!!!";
+    }
 }
