27cb7836bf141c6aa07b9818371f04ce97a9feb4
snaeja <sea28@hi.is>
2024-10-17 20:16:01 +0000

    jpa implementation

--- src/main/java/is/hi/hbv501g/hbv501g_h3/Services/Implementations/UserServiceImplementation.java
+++ src/main/java/is/hi/hbv501g/hbv501g_h3/Services/Implementations/UserServiceImplementation.java
@@ -1,54 +1,47 @@
 package is.hi.hbv501g.hbv501g_h3.Services.Implementations;
 
-import is.hi.hbv501g.hbv501g_h3.Persistence.Entities.Pattern;
+import is.hi.hbv501g.hbv501g_h3.Persistence.Repositories.UserRepository;
 import is.hi.hbv501g.hbv501g_h3.Services.UserService;
 import is.hi.hbv501g.hbv501g_h3.Persistence.Entities.User;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.ArrayList;
 
 @Service
 public class UserServiceImplementation implements UserService {
-    private List<User> userRepository = new ArrayList<>();
-    private Integer id_counter = 0;
+    private UserRepository userRepository;
 
-    public UserServiceImplementation() {
-        //Let chatGPT create 3 random users
-        User user1 = new User("knitMaster77", "securePass123", "knitmaster77@example.com",
-                new ArrayList<>(), new LinkedList<>());
-        User user2 = new User("patternQueen", "superSecret!456", "patternqueen@example.com",
-                new ArrayList<>(), new LinkedList<>());
-        User user3 = new User("craftyCat", "purrfectKnits", "craftycat@example.com",
-                new ArrayList<>(), new LinkedList<>());
+    @Autowired
+    public UserServiceImplementation(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-        userRepository.add(user1);
-        userRepository.add(user2);
-        userRepository.add(user3);
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 
-        //JPA gives each Pattern an ID, but here we add them manually
-        for(User u: userRepository){
-            u.setID((long) id_counter);
-            id_counter++;
-        }
+    @Override
+    public void delete(User user) {
+        userRepository.delete(user);
     }
 
     @Override
     public List<User> findAll(){
-        return userRepository;
+        return userRepository.findAll();
     }
 
 
     @Override
-    public User findByID(Long ID){
-        for(User u: userRepository){
-            if(u.getID().equals(ID)){
-                return u;
-            }
-        }
-        return null; //throw error instead of returning null
+    public User findById(Long ID){
+        //User user = userRepository.findById(ID).get();
+        return userRepository.findById(ID).orElse(null);
+    }
+
+    @Override
+    public List<User> findByUsername(String username){
+        return userRepository.findByUsername(username);
     }
 
 }
