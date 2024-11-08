34cca5fdccfcb8986eb5878558dc807fd9da2547
Elías <elias.ludviksson@gmail.com>
2024-11-08 21:31:43 +0000

    Opna CORS

--- src/main/java/is/hi/hbv501g/hbv501g_h3/Controllers/PatternController.java
+++ src/main/java/is/hi/hbv501g/hbv501g_h3/Controllers/PatternController.java
@@ -13,9 +13,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -23,6 +20,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/patterns")
+@CrossOrigin(origins = "*")
 public class PatternController {
 
     @Autowired
